d748ef259e2ed8c6e02818a93babe9df95bacf81
Manishankar Vakta <manishankarvakta@Manishankars-MacBook-Pro.local>
2024-10-21 07:01:17 +0600

    Update packege

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import SessionProvider from "@/providers/SessionProvider";
+import Header from "../_components/home/Header/Header";
+import Footer from "../_components/Shared/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
+          <Header />
           {children}
+          <Footer />
         </SessionProvider>
       </body>
     </html>
