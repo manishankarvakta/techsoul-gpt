29a27636980322dec7b8caef2f130430bb8a174a
Manishankar Vakta <manishankarvakta@Manishankars-MacBook-Pro.local>
2024-10-21 07:36:34 +0600

    Update eslint rouls

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,10 @@ const nextConfig = {
     }
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
