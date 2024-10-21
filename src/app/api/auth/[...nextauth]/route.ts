22065495a6d8031f31a292a3cdb4a37442ee08eb
Manishankar Vakta <manishankarvakta@Manishankars-MacBook-Pro.local>
2024-10-21 08:14:14 +0600

    packeges

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,49 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import GitHubProvider from "next-auth/providers/github";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-    }),
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { type: "email", placeholder: "Email address" },
-        password: { type: "password", placeholder: "Password" },
-      },
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      async authorize(credentials) {
-        const { email, password } = credentials;
-console.log(password)
-        const user = await prisma.user.findUnique({
-          where: { email },
-        });
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID || "",
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+//     }),
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID || "",
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+//     }),
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {
+//         email: { type: "email", placeholder: "Email address" },
+//         password: { type: "password", placeholder: "Password" },
+//       },
+//       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//       // @ts-ignore
+//       async authorize(credentials) {
+//         const { email, password } = credentials;
+// console.log(password)
+//         const user = await prisma.user.findUnique({
+//           where: { email },
+//         });
 
-        if (!user) {
-          throw new Error("Invalid email or password");
-        }
-        return { id: user.id, name: user.name, email: user.email };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-    SignUp: "/auth/signup",
-  },
-};
+//         if (!user) {
+//           throw new Error("Invalid email or password");
+//         }
+//         return { id: user.id, name: user.name, email: user.email };
+//       },
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/auth/login",
+//     SignUp: "/auth/signup",
+//   },
+// };
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
